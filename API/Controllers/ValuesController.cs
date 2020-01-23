45bd2734e9a50118551c54bc29a9a798299c7f36
Saif Abu Shamma <sabushamma@terkwaz.com>
2020-01-23 12:53:38 +0200

    End of Section 4

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Domain;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
